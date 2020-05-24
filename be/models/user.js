ac1089907d26ad622ee4c93912661d2a909b8e08
dbalabanova <60741570+dbalabanova@users.noreply.github.com>
2020-05-24 13:53:34 +0300

    final

--- be/models/user.js
+++ be/models/user.js
@@ -9,11 +9,7 @@ const userSchema = new Schema({
         unique: true,
         require: true
     },
-    // username: {
-    //     type: String,
-    //     unique: true,
-    //     require: true
-    // },
+  
     password: {
         type: String,
         require: true
