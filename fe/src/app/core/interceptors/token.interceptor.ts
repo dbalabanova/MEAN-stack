9d458cbfd3714cc6abb6472c64a3a6839e399e5a
dbalabanova <60741570+dbalabanova@users.noreply.github.com>
2020-05-24 13:55:46 +0300

    final

--- fe/src/app/core/interceptors/token.interceptor.ts
+++ fe/src/app/core/interceptors/token.interceptor.ts
@@ -4,24 +4,6 @@ import {Observable} from 'rxjs'
 import {Injectable} from '@angular/core'
 import { AuthService } from '../services/auth.service'
 
-// @Injectable( {
-//     providedIn:'root'
-// })
-// export class TokenInterceptor implements HttpInterceptor {
-//     constructor(private authService:AuthService){}
-//     intercept(req: HttpRequest<any>, next: HttpHandler)
-//     : Observable<HttpEvent<any>> {
-//         const token = this.authService.getToken()
-//         if(token){
-//             req=req.clone({
-//                 url: `${req.url}?auth=${token}`
-//             })
-//         }
-//         return next.handle(req)
-//     }
-    
-// }
-
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor { 
 
